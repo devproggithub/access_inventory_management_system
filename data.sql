0230a6a29851e24bf9dbea73db27adab6da2e578
YASSINE TALEB <137897741+devproggithub@users.noreply.github.com>
2024-12-29 15:48:37 +0100

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,9 @@
+INSERT INTO products (name, category, quantity, price)
+VALUES 
+    ('Telephone', 'Électronique', 10, 750),
+    ('Clavier Mécanique', 'Électronique', 25, 49.0),
+    ('Bouteille d\'eau', 'Accessoires', 50, 9.8),
+    ('Cahier', 'Papeterie', 8, 8.8),
+    ('Stylo', 'Papeterie', 7, 98.89);
+    
+    SELECT * FROM products;
