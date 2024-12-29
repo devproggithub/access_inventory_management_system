f63ce643e493a1b9e1f50595552ea6fbd069613e
YASSINE TALEB <137897741+devproggithub@users.noreply.github.com>
2024-12-29 15:45:06 +0100

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE Inventorydb;
+USE Inventorydb;
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,              
+    category VARCHAR(50),                    
+    quantity INT NOT NULL,                   
+    price DECIMAL(10, 2) NOT NULL            
+);
